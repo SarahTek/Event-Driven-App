6ee82dd046a293add288c613f8ad540bf2bfcd91
Sarah Teklemariam <sarah.f.tsion@gmail.com>
2022-09-04 23:30:26 -0400

    working on Lab 13

--- src/caps.js
+++ src/caps.js
@@ -6,36 +6,46 @@ const driver = io.of("./driver");
 
 
 
-function start() {
+// function start() {
+io.on("connection", (client) => {
 
-    io.on('connection', (client) => {
+
+
+    vendor.on('connection', (client) => {
         client.on('newOrder', (payload) => {
-            io.emit('orderForPickup', payload);
-        });
-        client.on('driver-picked-up', (payload) => {
+            console.log('SockettttttId:', client.id);
+            client.join(payload.store);
+            console.log('create a new room', payload.store);
             eventLogger(payload, 'pickUp');
-            io.emit('in-transit', payload);
-        });
-
-        client.on('package-in-transit', (payload) => {
-            eventLogger(payload, 'in-transit');
-            io.emit('order-delivered', payload);
-        });
-
-        client.on('package-delivered', (payload) => {
-            eventLogger(payload, 'delivered');
-            io.emit('delivered-confirmation', payload);
+            driver.emit('orderForPickup', payload);
         });
     });
 
-}
+    driver.on('connection', (client) => {
+        client.on('pickUp', (payload) => {
+            client.emit('in-transit', payload);
+        });
+        // eventLogger(payload, 'pickUp');
+
+        client.on('order-in-transit', (payload) => {
+            eventLogger(payload, 'in-transit');
+            client.emit('order-delivered', payload);
+        });
+
+        client.on('package-delivered', (payload) => {
+            setTimeout(() => {
+                eventLogger(payload, 'delivered');
+            }, 3000);
+            vendor.emit('delivered-confirmation', payload);
+        });
+    });
 
 
 
-function eventLogger(payload, str) {
-    const date = Date.now();
-    const todayTime = new Date(date).toUTCString();
-    console.log(`
+    function eventLogger(payload, str) {
+        const date = Date.now();
+        const todayTime = new Date(date).toUTCString();
+        console.log(`
       EVENT: {\n
           event: "${str}",\n
           time: "${todayTime}",\n
@@ -46,7 +56,7 @@ function eventLogger(payload, str) {
               address: "${payload.address}", \n
           },
       }`);
-}
+    }
+});
 
-
-start();
+        // start();
